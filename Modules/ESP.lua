a65554be94c43e5ed6a964d807c6c91fccd93fab
bolotnyx <zxcexpert1@gmail.com>
2025-12-27 13:20:43 +0300

    Update ESP.lua

--- Modules/ESP.lua
+++ Modules/ESP.lua
@@ -7,22 +7,20 @@ ESP.Enabled = false
 
 RunService.RenderStepped:Connect(function()
 	if not ESP.Enabled then return end
+	local enemiesFolder = workspace:FindFirstChild("Enemies") -- папка с NPC
+	if not enemiesFolder then return end
 
-	for _,v in ipairs(workspace:GetDescendants()) do
-		if v:IsA("Model") and v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") then
-			if not Players:GetPlayerFromCharacter(v) then
-				if v.Humanoid.Health > 0 then
-					if not v:FindFirstChild("ESP") then
-						local box = Instance.new("BoxHandleAdornment", v)
-						box.Name = "ESP"
-						box.Adornee = v
-						box.Size = v:GetExtentsSize()
-						box.Color3 = Color3.fromRGB(255,0,0)
-						box.Transparency = 0.6
-						box.AlwaysOnTop = true
-						box.ZIndex = 5
-					end
-				end
+	for _,v in ipairs(enemiesFolder:GetChildren()) do
+		if v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") then
+			if not v:FindFirstChild("ESP") then
+				local box = Instance.new("BoxHandleAdornment", v)
+				box.Name = "ESP"
+				box.Adornee = v
+				box.Size = v:GetExtentsSize()
+				box.Color3 = Color3.fromRGB(255,0,0)
+				box.Transparency = 0.6
+				box.AlwaysOnTop = true
+				box.ZIndex = 5
 			end
 		end
 	end
