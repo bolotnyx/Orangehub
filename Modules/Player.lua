73c7f2639807570b8fb1bf46c3269661d736624e
bolotnyx <zxcexpert1@gmail.com>
2025-12-27 13:21:16 +0300

    Refactor Player module variable names for consistency

--- Modules/Player.lua
+++ Modules/Player.lua
@@ -1,12 +1,12 @@
 -- Modules/Player.lua
 local Players = game:GetService("Players")
 local LP = Players.LocalPlayer
-local char = LP.Character or LP.CharacterAdded:Wait()
-local humanoid = char:WaitForChild("Humanoid")
+local Char = LP.Character or LP.CharacterAdded:Wait()
+local Hum = Char:WaitForChild("Humanoid")
 
 local PlayerFuncs = {}
-PlayerFuncs.Humanoid = humanoid
-PlayerFuncs.Speed = humanoid.WalkSpeed
-PlayerFuncs.JumpPower = humanoid.JumpPower
+PlayerFuncs.Humanoid = Hum
+PlayerFuncs.Speed = Hum.WalkSpeed
+PlayerFuncs.JumpPower = Hum.JumpPower
 
 return PlayerFuncs
