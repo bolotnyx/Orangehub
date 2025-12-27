7ce390bce0cea59959957313c17d2fb3240e93c1
bolotnyx <zxcexpert1@gmail.com>
2025-12-27 14:33:45 +0300

    Implement auto logging and key press functions
    Added functionality for automatic logging and key press simulation.

--- Modules/Player.lua
+++ Modules/Player.lua
@@ -1,12 +1,40 @@
--- Modules/Player.lua
 local Players = game:GetService("Players")
 local LP = Players.LocalPlayer
-local Char = LP.Character or LP.CharacterAdded:Wait()
-local Hum = Char:WaitForChild("Humanoid")
+local VIM = game:GetService("VirtualInputManager")
 
-local PlayerFuncs = {}
-PlayerFuncs.Humanoid = Hum
-PlayerFuncs.Speed = Hum.WalkSpeed
-PlayerFuncs.JumpPower = Hum.JumpPower
+local PlayerFuncs = {
+    AutoLog = false,
+    AutoTree = false,
+    LogBagType = 5 -- Вместимость мешка
+}
+
+-- Функция нажатия клавиш
+local function press(key)
+    VIM:SendKeyEvent(true, Enum.KeyCode[key], false, game)
+    task.wait(0.05)
+    VIM:SendKeyEvent(false, Enum.KeyCode[key], false, game)
+end
+
+-- Логика сбора логов (твой рабочий метод)
+task.spawn(function()
+    while true do
+        task.wait(1)
+        if PlayerFuncs.AutoLog and LP.Character and LP.Character:FindFirstChild("HumanoidRootPart") then
+            local log = nil
+            for _, v in pairs(workspace:GetDescendants()) do
+                if v.Name == "Log" then log = v; break end
+            end
+            
+            if log then
+                LP.Character:PivotTo(log:GetPivot() * CFrame.new(0, 2, 0))
+                task.wait(0.5)
+                for i=1, PlayerFuncs.LogBagType do
+                    press("F") press("E") task.wait(0.1)
+                end
+                LP.Character:PivotTo(CFrame.new(0, 10, 0)) -- К костру
+            end
+        end
+    end
+end)
 
 return PlayerFuncs
