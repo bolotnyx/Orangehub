07b51ceffa79aa0c2ca3497680ad641f44c4f25d
bolotnyx <zxcexpert1@gmail.com>
2025-12-27 12:43:01 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,1 @@
--- Loader.lua
--- Загружает core файл
-
-loadstring(game:HttpGet(
-"https://raw.githubusercontent.com/bolotnyx/OrangeHub/main/OrangeHub.lua"
-))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bolotnyx/Orangehub/main/Orangehub.lua"))()
