5006db3e52ce59ec375b2c808c48e54e8675dc8a
bolotnyx <zxcexpert1@gmail.com>
2025-12-28 16:46:56 +0300

    Update InfiniteJump.lua

--- Modules/InfiniteJump.lua
+++ Modules/InfiniteJump.lua
@@ -1,19 +1,39 @@
-local InfJumpModule = {
-    Enabled = false
-}
+local InfiniteJump = { Enabled = false }
 
-local LP = game.Players.LocalPlayer
+local Players = game:GetService("Players")
 local UIS = game:GetService("UserInputService")
 
--- Подключаем событие один раз при загрузке модуля
+local LP = Players.LocalPlayer
+
+function InfiniteJump.SetState(state)
+	InfiniteJump.Enabled = state
+end
+
+-- ПК (Space)
 UIS.JumpRequest:Connect(function()
-    if InfJumpModule.Enabled and LP.Character then
-        local hum = LP.Character:FindFirstChildOfClass("Humanoid")
-        if hum then
-            -- Принудительно заставляем персонажа прыгнуть
-            hum:ChangeState(Enum.HumanoidStateType.Jumping)
-        end
-    end
+	if not InfiniteJump.Enabled then return end
+
+	local char = LP.Character
+	local hum = char and char:FindFirstChildOfClass("Humanoid")
+
+	if hum then
+		hum:ChangeState(Enum.HumanoidStateType.Jumping)
+	end
 end)
 
-return InfJumpModule
+-- Android (кнопка прыжка)
+task.spawn(function()
+	while true do
+		if InfiniteJump.Enabled then
+			local char = LP.Character
+			local hum = char and char:FindFirstChildOfClass("Humanoid")
+
+			if hum and hum.Jump then
+				hum:ChangeState(Enum.HumanoidStateType.Jumping)
+			end
+		end
+		task.wait(0.1)
+	end
+end)
+
+return InfiniteJump
