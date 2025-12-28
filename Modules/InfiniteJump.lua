c15e5932dbce9d3277f97ca6eb1fa63c7dd338f5
bolotnyx <zxcexpert1@gmail.com>
2025-12-28 16:58:34 +0300

    Update InfiniteJump.lua

--- Modules/InfiniteJump.lua
+++ Modules/InfiniteJump.lua
@@ -1,7 +1,7 @@
 local InfiniteJump = { Enabled = false }
 
 local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
+local UIS = game:GetService("UserInputService")
 
 local LP = Players.LocalPlayer
 
@@ -9,17 +9,13 @@ function InfiniteJump.SetState(state)
 	InfiniteJump.Enabled = state
 end
 
--- основной цикл
-RunService.Heartbeat:Connect(function()
+UIS.JumpRequest:Connect(function()
 	if not InfiniteJump.Enabled then return end
 
 	local char = LP.Character
 	local hum = char and char:FindFirstChildOfClass("Humanoid")
 
-	if not hum then return end
-
-	-- если игрок в воздухе и нажимает прыжок — разрешаем прыжок снова
-	if hum:GetState() == Enum.HumanoidStateType.Freefall and hum.Jump then
+	if hum then
 		hum:ChangeState(Enum.HumanoidStateType.Jumping)
 	end
 end)
