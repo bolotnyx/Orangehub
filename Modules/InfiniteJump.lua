557d4949639f90becda5c0cdab38f414582da9d5
bolotnyx <zxcexpert1@gmail.com>
2025-12-28 17:04:11 +0300

    Update InfiniteJump.lua

--- Modules/InfiniteJump.lua
+++ Modules/InfiniteJump.lua
@@ -1,23 +1,6 @@
-local InfiniteJump = {}
-InfiniteJump.Enabled = false
-
-local UIS = game:GetService("UserInputService")
-local Players = game:GetService("Players")
-
-UIS.JumpRequest:Connect(function()
-	if InfiniteJump.Enabled then
-		local char = Players.LocalPlayer.Character
-		if char then
-			local hum = char:FindFirstChildOfClass("Humanoid")
-			if hum then
-				hum:ChangeState("Jumping")
-			end
-		end
+local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
 	end
 end)
-
-function InfiniteJump.SetState(state)
-	InfiniteJump.Enabled = state
-end
-
-return InfiniteJump
