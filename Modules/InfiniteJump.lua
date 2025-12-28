c65be2426017b26b4533684b34c1ef152004444a
bolotnyx <zxcexpert1@gmail.com>
2025-12-28 13:52:54 +0300

    Update InfiniteJump.lua

--- Modules/InfiniteJump.lua
+++ Modules/InfiniteJump.lua
@@ -6,10 +6,13 @@ local UserInputService = game:GetService("UserInputService")
 local LP = game.Players.LocalPlayer
 
 UserInputService.JumpRequest:Connect(function()
-    if InfJumpModule.Enabled and LP.Character then
-        local hum = LP.Character:FindFirstChildOfClass("Humanoid")
-        if hum then
-            hum:ChangeState("Jumping")
+    if InfJumpModule.Enabled then
+        local character = LP.Character
+        local humanoid = character and character:FindFirstChildOfClass("Humanoid")
+        
+        if humanoid then
+            -- Даем персонажу импульс прыжка, даже если он в воздухе
+            humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
         end
     end
 end)
